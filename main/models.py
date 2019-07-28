ab220fdaf30d9db04116b4166ffcdeb6c2f75cb4
DESKTOP-UJSG353\andyh <andyh1393@gmail.com>
2019-07-28 10:48:39 +1000

    set up initial model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class PageCategory(models.Model):
+    page_category = models.CharField(max_length=200)
+    category_blurb = models.CharField(max_length=200)
+    category_slug = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name_plural = "Page Categories"
+
+    def __str__(self):
+        return self.page_category
+
+
+# class LifestyleCategory(models.Model):
+#     lifestyle_category = models.CharField(max_length=200)
+#     category_summary = models.CharField(max_length=200)
